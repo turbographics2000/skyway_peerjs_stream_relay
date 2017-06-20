63f878c410c66f1f091d0a63a2c0a83c7d739067
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-06-20 20:22:04 +0900

    commit

--- docs/test.js
+++ docs/test.js
@@ -7,82 +7,85 @@ var peer = null;
 
 PeerClassExtend();
 
+btnRootStart.style.display = btnStart.style.display = '';
 btnRootStart.onclick = evt => {
     peer = new Peer('root', { key: apiKey, debug: 3 });
-    peerInstanceExtend(peer);
+    peerSetup();
 }
-btnStart.style.display = '';
 btnStart.onclick = evt => {
     peer = new Peer({ key: apiKey, debug: 3 });
-    peerInstanceExtend(peer);
+    peerSetup();
 }
 
+function peerSetup() {
+    peerInstanceExtend(peer);
 
-peer.on('open', id => {
-    console.log('peer on "open"');
-    myIdDisp.textContent = myId = id;
-    if (id === 'root') {
-        webCamSetup(selfView).then(_ => { });
-    } else {
-        notifyJoin();
-    }
-    myIdDisp.textContent = id;
-});
+    peer.on('open', id => {
+        console.log('peer on "open"');
+        myIdDisp.textContent = myId = id;
+        if (id === 'root') {
+            webCamSetup(selfView).then(_ => { });
+        } else {
+            notifyJoin();
+        }
+        myIdDisp.textContent = id;
+    });
 
-peer.on('call', call => {
-    addLogMsg('peer on "call"');
-    call.answer(null);
-    callSetup(call);
-});
+    peer.on('call', call => {
+        addLogMsg('peer on "call"');
+        call.answer(null);
+        callSetup(call);
+    });
 
-// 新規視聴者(ブランチ)が参加したとき、放送主側で発生するイベント
-peer.on('join', joinId => {
-    addLogMsg('join', 'event');
-    var branchData;
-    // 視聴者(ブランチ)配置する
-    if (levelBranches[0] === undefined) {
-        branchData = initBranch(joinId);
-    } else {
-        branchData = addBranch(joinId);
-    }
-    // 配置結果を視聴者に知らせる
-    peer.notifyBranchData(branchData);
-});
+    // 新規視聴者(ブランチ)が参加したとき、放送主側で発生するイベント
+    peer.on('join', joinId => {
+        addLogMsg('join', 'event');
+        var branchData;
+        // 視聴者(ブランチ)配置する
+        if (levelBranches[0] === undefined) {
+            branchData = initBranch(joinId);
+        } else {
+            branchData = addBranch(joinId);
+        }
+        // 配置結果を視聴者に知らせる
+        peer.notifyBranchData(branchData);
+    });
 
-// 配置結果を受信したとき視聴者(ブランチ)側で発生するイベント
-peer.on('branch_data', data => {
-    addLogMsg('branch_data', 'event');
-    branchData = data;
-    if (branchSrcConnection) {
-        branchSrcConnection.close();
-        branchSrcConnection = null;
-    }
-    // if (branchConnections) {
-    //     Object.keys(branchConnections).forEach(branchId => {
-    //         branchConnections[branchId].close();
-    //         delete branchConnections[branchId];
-    //     });
-    // }
-    peer.requestBranch();
-});
+    // 配置結果を受信したとき視聴者(ブランチ)側で発生するイベント
+    peer.on('branch_data', data => {
+        addLogMsg('branch_data', 'event');
+        branchData = data;
+        if (branchSrcConnection) {
+            branchSrcConnection.close();
+            branchSrcConnection = null;
+        }
+        // if (branchConnections) {
+        //     Object.keys(branchConnections).forEach(branchId => {
+        //         branchConnections[branchId].close();
+        //         delete branchConnections[branchId];
+        //     });
+        // }
+        peer.requestBranch();
+    });
 
-// ブランチからストリームの送信をリクエストしたときにブランチ元(ブランチソース)側で発生するイベント
-peer.on('request_branch', branchId => {
-    addLogMsg('request_branch', 'event');
-    branchConnections[branchId] = peer.call(branchId, stream);
-});
+    // ブランチからストリームの送信をリクエストしたときにブランチ元(ブランチソース)側で発生するイベント
+    peer.on('request_branch', branchId => {
+        addLogMsg('request_branch', 'event');
+        branchConnections[branchId] = peer.call(branchId, stream);
+    });
 
-// 視聴者(ブランチ)が視聴をやめたとき(close)、
-// 視聴をやめたブランチをブランチ元(ブランチソース)が放送主(ルート)に報告する。
-// その報告を受信したとき放送主(ルート)側で発生するイベント
-peer.on('close_branch', closeId => {
-    console.log('peer on "close_branch"');
-    if (closeNotifiyIgnoreIds[remoteId]) {
-        delete closeNotifiyIgnoreIds[remoteId];
-        return;
-    }
-    peer.migrateBranch(closeId);
-});
+    // 視聴者(ブランチ)が視聴をやめたとき(close)、
+    // 視聴をやめたブランチをブランチ元(ブランチソース)が放送主(ルート)に報告する。
+    // その報告を受信したとき放送主(ルート)側で発生するイベント
+    peer.on('close_branch', closeId => {
+        console.log('peer on "close_branch"');
+        if (closeNotifiyIgnoreIds[remoteId]) {
+            delete closeNotifiyIgnoreIds[remoteId];
+            return;
+        }
+        peer.migrateBranch(closeId);
+    });
+}
 
 function webCamSetup(elm) {
     return navigator.mediaDevices.getUserMedia({
