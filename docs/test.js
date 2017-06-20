4e75929108f01bf6b3e33bd24e4d3cd2210fdf83
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-06-21 00:10:14 +0900

    commit

--- docs/test.js
+++ docs/test.js
@@ -55,7 +55,7 @@ function peerSetup() {
     // 配置結果を受信したとき視聴者(ブランチ)側で発生するイベント
     peer.on('branch_data', data => {
         addLogMsg('branch_data', 'event');
-        branchData = data;
+        peer.branchData = data;
         if (peer.branchSrcConnection) {
             peer.branchSrcConnection.close();
             peer.branchSrcConnection = null;
