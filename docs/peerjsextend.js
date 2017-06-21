f543f3f75345189e712911b80ba5e695b63234ed
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-06-22 00:11:57 +0900

    commit

--- docs/peerjsextend.js
+++ docs/peerjsextend.js
@@ -125,47 +125,69 @@ function PeerClassExtend() {
     };
 
     Peer.prototype.drawTree = function () {
-        var data = this.levelBranches.reduce((a, b) => {
-            return Object.assign(a, b);
-        }, {});
         var boxWidth = 150,
             boxHeight = 40;
-        var zoom = d3.zoom()
+
+        // Setup zoom and pan
+        var zoom = d3.behavior.zoom()
             .scaleExtent([.1, 1])
-            .on('zoom', _ => {
+            .on('zoom', function () {
                 svg.attr('transform', 'translate(' + d3.event.translate + ') scale(' + d3.event.scale + ')');
-            });
+            })
+            // Offset so that first pan and zoom does not jump back to the origin
+            .translate([150, 200]);
 
         var svg = d3.select('body').append('svg')
             .attr('width', 1000)
             .attr('height', 500)
             .call(zoom)
             .append('g')
+            // Left padding of tree so that the whole root node is on the screen.
+            // TODO: find a better way
             .attr('transform', 'translate(150,200)');
 
-        var tree = d3.tree()
+        var tree = d3.layout.tree()
+            // Using nodeSize we are able to control
+            // the separation between nodes. If we used
+            // the size parameter instead then d3 would
+            // calculate the separation dynamically to fill
+            // the available space.
             .nodeSize([100, 200])
-            .separation(_ => .5)
-            .children(branchData => branchData.children);
+            // By default, cousins are drawn further apart than siblings.
+            // By returning the same value in all cases, we draw cousins
+            // the same distance apart as siblings.
+            .separation(function () {
+                return .5;
+            })
+            // Tell d3 what the child nodes are. Remember, we're drawing
+            // a tree so the ancestors are child nodes.
+            .children(function (person) {
+                return person._parents;
+            });
 
-        var nodes = tree.nodes(data);
-        var links = tree.links(nodes);
 
+        var data = this.levelBranches.reduce((a, b) => {
+            return Object.assign(a, b);
+        }, {});
 
+        var nodes = tree.nodes(data),
+            links = tree.links(nodes);
+
+        // Style links (edges)
         svg.selectAll('path.link')
             .data(links)
-            .enter()
-            .append('path')
+            .enter().append('path')
             .attr('class', 'link')
             .attr('d', elbow);
 
-        var node = svg.selectAll('g.branch')
+        // Style nodes    
+        var node = svg.selectAll('g.person')
             .data(nodes)
-            .enter()
-            .append('g')
-            .attr('class', 'branch')
-            .attr('transform', d => 'translate(' + d.y + ',' + d.x + ')');
+            .enter().append('g')
+            .attr('class', 'person')
+            .attr('transform', function (d) { return 'translate(' + d.y + ',' + d.x + ')'; });
 
+        // Draw the rectangle person boxes
         node.append('rect')
             .attr({
                 x: -(boxWidth / 2),
@@ -174,19 +196,27 @@ function PeerClassExtend() {
                 height: boxHeight
             });
 
+        // Draw the person's name and position it inside the box
         node.append('text')
             .attr('dx', -(boxWidth / 2) + 10)
             .attr('dy', 0)
             .attr('text-anchor', 'start')
             .attr('class', 'name')
-            .text(d => d.id);
+            .text(function (d) {
+                return d.name;
+            });
 
+
+        /**
+         * Custom path function that creates straight connecting lines.
+         */
         function elbow(d) {
             return 'M' + d.source.y + ',' + d.source.x
                 + 'H' + (d.source.y + (d.target.y - d.source.y) / 2)
                 + 'V' + d.target.x
                 + 'H' + d.target.y;
         }
+
     }
 }
 
