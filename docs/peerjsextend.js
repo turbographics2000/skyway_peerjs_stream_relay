cad5fce1f69864be5e80deaa0e993345da157481
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-06-24 22:13:35 +0900

    commit

--- docs/peerjsextend.js
+++ docs/peerjsextend.js
@@ -334,7 +334,7 @@ function getTestPatternStream(displayTime) {
             var ctx = cnv.getContext('2d');
             ctx.textBaseline = 'middle';
             ctx.textAlign = 'center';
-            ctx.font = '30px monospace';
+            ctx.font = '30px bold monospace';
             ctx.fillStyle = 'white';
             var rafId = null;
             var img = document.createElement('img');
